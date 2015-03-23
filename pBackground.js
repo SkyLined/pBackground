d6310bd6d86888e3e3f39d3b1fb39529262e4d71
SkyLined <berendjanwever@gmail.com>
2015-03-23 16:29:23 +0100

    Allow child process to write to stdio
    * The target process running in the background can now write to stdout
    without terminating because an intermediate process is created that will
    read and discard any data written to it.

--- pBackground.js
+++ pBackground.js
@@ -1,16 +1,29 @@
+var mForeverMonitor = require('forever-monitor');
+// If the main process spawns a child and exists, the child cannot write
+// anything to stdio or it will terminate. forever-monitor can surpress stdio
+// but does not to be actively running in the parent process to do so.
+// Since the main process must exit for the child to run in the background,
+// a third intermediate process is needed to surpress the output. So, the
+// main process spawns the intermediate process and exits, the intermediate
+// process spawns the target process and surpresses its output. The target can
+// write to stdio without terminating - the output will simply be ignored.
+
+sThisScriptPath = process.argv[1];
 asArguments = process.argv.slice(2);
 if (asArguments.length == 0) {
   console.log("Usage: node pBackground.js script-to-run-in-background.js [arguments]");
+} else if (asArguments[0] == "--child") {
+  var oChild = mForeverMonitor.start(asArguments[1], {
+    "args": asArguments.splice(2),
+    "silent": "true"
+  });
 } else {
   bForever = asArguments[0] == "--forever";
-  if (bForever) asArguments = asArguments.splice(1);
-  var mForeverMonitor = require('forever-monitor');
-  var oChild = mForeverMonitor.start(
-    asArguments[0], {
-      "args": asArguments.splice(1),
-      "max": bForever ? undefined : 1
-    }
-  );
+  if (bForever) asArguments = asArguments.shift();
+  var oChild = mForeverMonitor.start(sThisScriptPath, {
+    "args": ["--child"].concat(asArguments),
+    "max": bForever ? undefined : 1
+  });
   console.log("Started process 0x" + oChild.child.pid.toString(16) + "/" + oChild.child.pid);
+  process.exit();
 }
-process.exit();
---
+++ test.js
@@ -0,0 +1,2 @@
+console.log("Hello, world!");
+require("fs").writeFile("test.txt", "Hello, world!");
